2e9d6139115503e0f0ca0134fdf13ee921c9e262
Carl Ekerot <carl.ekerot@lunicore.se>
2013-01-07 15:13:22 +0100

    Fixed bin_tool

--- code/bin_tool.py
+++ code/bin_tool.py
@@ -1,10 +1,15 @@
+#! /usr/bin/env python
+
+import sys
 
 def to_bin(s):
     line = ""
     for i in range(0, len(s), 2):
         if i % 8 == 0:
-            print line
-            line = ""
+            line += "\n"
         byte = s[i] + s[i+1]
         binstr = bin(int(byte, 16))[2:]
         line += '%08d ' % int(binstr)
+    print(line)
+
+to_bin(sys.argv[1])
