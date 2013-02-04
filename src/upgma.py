3996ffac050f6e470ed333e2b8f9be71feba85c2
Fredrik Appelros <fredrik.appelros@gmail.com>
2013-02-04 15:19:48 +0000

    UPGMA with byte values as features

--- src/upgma.py
+++ src/upgma.py
@@ -7,9 +7,11 @@ import align
 import numpy
 import itertools
 import pydot
+import collections
+import matplotlib.pyplot
 
-size = 100
-limit = 2000
+size = 6
+limit = 100
 M = numpy.ndarray((size, size))
 
 class Node(object):
@@ -21,7 +23,8 @@ class Node(object):
         for (i, j) in ab:
             if i > j:
                 (i, j) = (j, i)
-            score += M[i.index, j.index]
+            min_len = min(len(i.features), len(j.features))
+            score += numpy.linalg.norm(i.features[:min_len] - j.features[:min_len])
         score /= len(ab)
         return score
 
@@ -35,41 +38,57 @@ class ParentNode(Node):
         return self.left.get_leaves() + self.right.get_leaves()
 
 class LeafNode(Node):
-    def __init__(self, index):
+    def __init__(self, index, features):
         self.index = index
+        self.features = features
 
     def get_leaves(self):
         return [self]
 
-packets = pcap_reassembler.load_pcap('../smbtorture.cap')
+packets = pcap_reassembler.load_pcap('../cap/http.cap', strict=False)
+#streams = collections.defaultdict(list)
+#for packet in packets:
+#    streams[(packet.source_addr, packet.destination_addr)].append(packet.data)
+#for key in streams:
+#    print(key)
+entropy = collections.defaultdict(set)
+for packet in packets:
+    for (pos, val) in enumerate(packet.data[:limit]):
+        entropy[pos].add(val)
+#left = []
+#height = []
+#for pos in entropy:
+#    left.append(pos)
+#    height.append(len(entropy[pos]))
+#plot = matplotlib.pyplot.bar(left, height, color='r')
+#matplotlib.pyplot.show()
+
 msgs = map(lambda x: x.data[:limit], packets[:size])
 
 S = 3 * numpy.identity(256) - numpy.ones((256, 256))
 S = S.astype(numpy.int16)
 
-# only the upper diagonal part of this matrix is needed, build it smarter
-for i in range(size):
-    for j in range(i + 1, size):
-        (s, _, _) = align.align(msgs[i], msgs[j], -2, S, local=True)
-        M[i,j] = s
+#for i in range(size):
+#    for j in range(i + 1, size):
+#        (s, _, _) = align.align(msgs[i], msgs[j], -2, S, local=True)
+#        M[i,j] = s
 
-# input all the leaf nodes to this list as a start then remove and add parent nodes
-# as the tree is built; when one node is left the tree is built
 to_process = []
 for i in range(size):
-    to_process.append(LeafNode(i))
+    features = numpy.asarray(map(lambda (j, x): ord(x) / len(entropy[j]), enumerate(msgs[i])))
+    to_process.append(LeafNode(i, features))
 
 while len(to_process) > 1:
-    score_max = -1
+    score_min = float('inf')
     for (A, B) in itertools.combinations(to_process, 2):
         score = A.compare_to(B)
-        if score > score_max:
+        if score < score_min:
             to_merge = (A, B)
-            score_max = score
+            score_min = score
     (A, B) = to_merge
     to_process.remove(A)
     to_process.remove(B)
-    to_process.append(ParentNode(A, B, score_max))
+    to_process.append(ParentNode(A, B, score_min))
 
 count = 0
 def build_tree(tree, node, parent):
@@ -77,7 +96,7 @@ def build_tree(tree, node, parent):
     if type(node) is ParentNode:
         n = pydot.Node(str(count), shape='box', label=('Score: %.2f' % node.score))
     else:
-        n = pydot.Node(str(count), label=str(node.index))
+        n = pydot.Node(str(count), label=packets[node.index].number)
     count += 1
     tree.add_node(n)
     if parent:
