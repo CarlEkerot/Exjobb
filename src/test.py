677f26a23dd744293814846991a141afa9c2d7ab
Fredrik Appelros <fredrik.appelros@gmail.com>
2013-04-11 09:35:00 +0000

    RANSAC graphs

--- src/test.py
+++ src/test.py
@@ -1,21 +1,52 @@
 #! /usr/bin/env python
 
-import pcap_glue
-import protocol_analyser
 import pcap_reassembler
+import numpy as np
+import matplotlib.pyplot as plt
 
-packets = pcap_reassembler.load_pcap('../cap/dns-30628-packets.pcap', strict=True)
-nums = [p.number for p in packets]
-truth = []
-with open('../cap/dns.csv.clean') as f:
-    for line in f:
-        (no, type_) = line.split(',')
-        if int(no) in nums:
-            truth.append(type_[:-1])
+from align import string_to_alignment
 
-msgs = pcap_glue.build_messages('../cap/dns-30628-packets.pcap')
-an = protocol_analyser.ProtocolAnalyser(msgs[:20000], 100, truth)
-an.cluster(200, max_num_types=10)
-#an.state_inference('dns-state_diagram.png', 5)
-#an.classify_fields()
+def _get_value_from_bytes(data):
+    value = 0
+    for byte in data:
+        value = (value << 8) + byte
+    return value
+
+packets = pcap_reassembler.load_pcap('../cap/smb-only.cap', strict=True)
+msgs = [string_to_alignment(p.payload) for p in packets]
+
+from ransac import LineModel, ransac
+_model = LineModel()
+
+X = [_get_value_from_bytes(msg[0:4]) for msg in msgs]
+Y = [len(msg) for msg in msgs]
+data = np.asarray([X, Y]).T
+
+(params, _, res) = ransac(data, _model, 2, 0.5, 100)
+k = params[0]
+m = params[1]
+
+plt.plot(X, Y, 'ko')
+X = np.asarray([0, max(X)])
+Y = params[0] * X + params[1]
+plt.plot(X, Y, 'g-', linewidth=2)
+plt.xlim(0, 400)
+plt.ylim(0, 400)
+plt.show()
+
+X = [msg[36] for msg in msgs]
+Y = [len(msg) for msg in msgs]
+data = np.asarray([X, Y]).T
+
+(params, _, res) = ransac(data, _model, 2, 0.5, 100)
+k = params[0]
+m = params[1]
+
+plt.plot(X, Y, 'ko')
+X = np.asarray([0, max(X)])
+Y = params[0] * X + params[1]
+plt.plot(X, Y, 'g-', linewidth=2)
+plt.xlim(0, 50)
+plt.ylim(39, 139)
+plt.show()
 
