aac5c918821a6423adbabce826e91626f774c401
Carl Ekerot <carl.ekerot@lunicore.se>
2013-05-07 11:53:43 +0200

    Updated length plot. Added pdf.

--- src/test.py
+++ src/test.py
@@ -17,7 +17,7 @@ msgs = [string_to_alignment(p.payload) for p in packets]
 
 from ransac import LineModel, ransac
 _model = LineModel()
-
+"""
 X = [_get_value_from_bytes(msg[0:4]) for msg in msgs]
 Y = [len(msg) for msg in msgs]
 data = np.asarray([X, Y]).T
@@ -33,7 +33,7 @@ plt.plot(X, Y, 'g-', linewidth=2)
 plt.xlim(0, 400)
 plt.ylim(0, 400)
 plt.show()
-
+"""
 X = [msg[36] for msg in msgs]
 Y = [len(msg) for msg in msgs]
 data = np.asarray([X, Y]).T
@@ -42,11 +42,14 @@ data = np.asarray([X, Y]).T
 k = params[0]
 m = params[1]
 
-plt.plot(X, Y, 'ko')
+# plt.plot(X, Y, 'bo', edgecolor='b')
+plt.scatter(X, Y, s=30, facecolor='b', lw=0)
 X = np.asarray([0, max(X)])
 Y = params[0] * X + params[1]
-plt.plot(X, Y, 'g-', linewidth=2)
+plt.plot(X, Y, 'k-', linewidth=2)
 plt.xlim(0, 50)
 plt.ylim(39, 139)
+plt.xlabel("Byte value")
+plt.ylabel("Message length")
 plt.show()
 
